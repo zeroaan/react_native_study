9a140d314d518fbcc6862c605e926d5cda9569a9
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-31 20:01:14 +0900

    expo-location

--- rn_test/App.js
+++ rn_test/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Alert} from 'react-native';
+import * as Location from 'expo-location';
 
 import Loading from './Loading';
 
 const App = () => {
-  return (
-    <>
-      <Loading />
-    </>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      try {
+        await Location.requestPermissionsAsync();
+        const {
+          coords: {latitude, longitude},
+        } = await Location.getCurrentPositionAsync();
+        setIsLoading(false);
+      } catch (error) {
+        Alert.alert("Can't find you.", 'So sad');
+      }
+    };
+    getLocation();
+  }, []);
+
+  return <>{isLoading ? <Loading /> : null}</>;
 };
 
 export default App;
